93b2c69af5570a25ca7efa9ef6ed266821054dd8
saikumar <44186538+sk1998@users.noreply.github.com>
2019-08-08 13:18:19 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 
 int main() {
-  int x,p,s=0,y;
+  long int x,p,s=0,y;
   cin>>x>>p;
   while(x!=1)
   {
   s=s+x;      
   y=ceil((x*p)/100);
   x=x-y;
-  
   }
   cout<<s+1;
 }
